d3d77bd21932983f31c5c5915e4f4be963a738e7
HellenBorba <hellenmborba@icloud.com>
2022-04-25 17:06:07 -0300

    A

---
+++ Projeto_Pi/Assets/Scripts/Player.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public Rigidbody body;
+    
+    private float horizontal, vertical;
+    
+    void Start()
+    {
+        Cursor.visible = false;
+        body = GetComponent<Rigidbody>();
+    }
+    void Update()
+    {
+        horizontal = Input.GetAxis("Horizontal");
+        vertical = Input.GetAxis("Vertical");
+        body.velocity = new Vector3(horizontal * 10, 0, vertical * 10);
+    }
+}
