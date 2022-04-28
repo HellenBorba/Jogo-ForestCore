dffa5373bfa9e812e52f14dff514f1164cb77218
HellenBorba <hellenmborba@icloud.com>
2022-04-28 17:07:00 -0300

    B
    GameController;
    itensCollect.

--- Projeto_Pi/Assets/Scripts/GameContoller.cs
+++ Projeto_Pi/Assets/Scripts/GameContoller.cs
@@ -1,18 +1,55 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class GameContoller : MonoBehaviour
 {
+    public GameObject[] buton;
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Cursor.visible = true;
+    }
+    public void seleciono(int ButtonTipo)
+    {
+        switch (ButtonTipo)
+        {
+            case 0:
+                var colors = buton[0].GetComponent<Button>().colors;
+                colors.normalColor = Color.green;
+                buton[0].GetComponent<Button>().colors = colors;
+                break;
+            case 1:
+                var colors1 = buton[1].GetComponent<Button>().colors;
+                colors1.normalColor = Color.green;
+                buton[1].GetComponent<Button>().colors = colors1;
+                break;
+            case 2:
+                var colors2 = buton[2].GetComponent<Button>().colors;
+                colors2.normalColor = Color.green;
+                buton[2].GetComponent<Button>().colors = colors2;
+                break;
+            case 3:
+                var colors3 = buton[3].GetComponent<Button>().colors;
+                colors3.normalColor = Color.green;
+                buton[3].GetComponent<Button>().colors = colors3;
+                buton[4].SetActive(true);
+                break;
+        }
+    }
+    public void End(int scroollbar)
+    {
+        if (scroollbar == 1)
+        {
+            buton[5].SetActive(true);
+        }
     }
 }
--- Projeto_Pi/Assets/Scripts/ItemCollect.cs
+++ Projeto_Pi/Assets/Scripts/ItemCollect.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public enum tipoItem
@@ -12,6 +13,9 @@ public class ItemCollect : MonoBehaviour
 {
     public GameObject panel;
     public int Itemtipo;
+
+    private Scrollbar scrollbar;
+    private int id;
     // Start is called before the first frame update
     void Start()
     {
@@ -24,7 +28,7 @@ public class ItemCollect : MonoBehaviour
       
 
     }
-   
+         
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.CompareTag("Player"))
@@ -32,7 +36,15 @@ public class ItemCollect : MonoBehaviour
             switch (Itemtipo)
             {
                 case 0:
-                    SceneManager.LoadSceneAsync(0);
+                    //SceneManager.LoadScene (1, LoadSceneMode.Additive);
+                    panel.SetActive(true);
+                    break;
+                case 1:
+                    panel.SetActive(true);
+                    scrollbar.value = id;
+                    break;
+                case 2:
+                    panel.SetActive(true);
                     break;
             }
         }
