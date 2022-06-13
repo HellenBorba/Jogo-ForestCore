250a6a97b5babe7320e5ba0f4a511f1a3f9cada9
HellenBorba <hellenmborba@icloud.com>
2022-06-13 17:07:58 -0300

    O
    Interface;
    GameControllerCHO;
    GameController.

---
+++ Projeto_Pi/Assets/Scripts/GameControllerCHO.cs
@@ -0,0 +1,250 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameControllerCHO : MonoBehaviour
+{
+    public GameObject[] Objeto;
+    public GameObject pontoFixoH, pontoFixoO, pontoFixoC, Oxigenio, Hidrogenio, Carbono;
+    public Color green;
+    public string valor;
+
+    [SerializeField]
+    private int vl;
+    //----------------------------------------------------------------------------------------------------------------------------------------
+    void Start()
+    {
+
+    }
+    //----------------------------------------------------------------------------------------------------------------------------------------
+    void Update()
+    {
+
+    }
+    //----------------------------------------------------------------------------------------------------------------------------------------0-5-8-11-14-17
+    #region H
+    public void BotãoH()
+    {
+        vl += 1;
+        switch (vl)
+        {
+            case 1:
+                Hidrogenio.transform.position = Objeto[0].transform.position;
+                if (Hidrogenio.transform.position == Objeto[0].transform.position)
+                {
+                    Color a = green;
+                    Objeto[0].GetComponent<SpriteRenderer>().color = a;
+                }
+                break;
+            case 2:
+                Hidrogenio.transform.position = pontoFixoH.transform.position;
+                break;
+            case 11:
+                Hidrogenio.transform.position = Objeto[5].transform.position;
+                if(Hidrogenio.transform.position == Objeto[5].transform.position)
+                {
+                    Color b = green;
+                    Objeto[5].GetComponent<SpriteRenderer>().color = b;
+                }
+                break;
+            case 12:
+                Hidrogenio.transform.position = pontoFixoH.transform.position;
+                break;
+            case 17:
+                Hidrogenio.transform.position = Objeto[8].transform.position;
+                if(Hidrogenio.transform.position == Objeto[8].transform.position)
+                {
+                    Color c = green;
+                    Objeto[8].GetComponent<SpriteRenderer>().color = c;
+                }
+                break;
+            case 18:
+                Hidrogenio.transform.position = pontoFixoH.transform.position;
+                break;
+            case 23:
+                Hidrogenio.transform.position = Objeto[11].transform.position;
+                if(Hidrogenio.transform.position == Objeto[11].transform.position)
+                {
+                    Color d = green;
+                    Objeto[8].GetComponent<SpriteRenderer>().color = d;
+                }
+                break;
+            case 24:
+                Hidrogenio.transform.position = pontoFixoH.transform.position;
+                break;
+            case 29:
+                Hidrogenio.transform.position = Objeto[14].transform.position;
+                if (Hidrogenio.transform.position == Objeto[14].transform.position)
+                {
+                    Color e = green;
+                    Objeto[14].GetComponent<SpriteRenderer>().color = e;
+                }
+                break;
+            case 30:
+                Hidrogenio.transform.position = pontoFixoH.transform.position;
+                break;
+            case 35:
+                Hidrogenio.transform.position = Objeto[17].transform.position;
+                if (Hidrogenio.transform.position == Objeto[17].transform.position)
+                {
+                    Color f = green;
+                    Objeto[17].GetComponent<SpriteRenderer>().color = f;
+                }
+                break;
+            case 36:
+                Hidrogenio.transform.position = pontoFixoH.transform.position;
+                break;
+        }
+    }
+    #endregion
+    //----------------------------------------------------------------------------------------------------------------------------------------1-4-7-10-13-16
+    #region O
+    public void BotãoO()
+    {
+        switch (vl)
+        {
+            case 3:
+                Oxigenio.transform.position = Objeto[1].transform.position;
+                if (Oxigenio.transform.position == Objeto[1].transform.position)
+                {
+                    Color a = green;
+                    Objeto[1].GetComponent<SpriteRenderer>().color = a;
+                }
+                break;
+            case 4:
+                Oxigenio.transform.position = pontoFixoO.transform.position;
+                break;
+            case 9:
+                Oxigenio.transform.position = Objeto[4].transform.position;
+                if (Oxigenio.transform.position == Objeto[4].transform.position)
+                {
+                    Color b = green;
+                    Objeto[4].GetComponent<SpriteRenderer>().color = b;
+                }
+                break;
+            case 10:
+                Oxigenio.transform.position = pontoFixoO.transform.position;
+                break;
+            case 15:
+                Oxigenio.transform.position = Objeto[7].transform.position;
+                if (Oxigenio.transform.position == Objeto[7].transform.position)
+                {
+                    Color c = green;
+                    Objeto[7].GetComponent<SpriteRenderer>().color = c;
+                }
+                break;
+            case 16:
+                Oxigenio.transform.position = pontoFixoO.transform.position;
+                break;
+            case 21:
+                Oxigenio.transform.position = Objeto[10].transform.position;
+                if (Oxigenio.transform.position == Objeto[10].transform.position)
+                {
+                    Color d = green;
+                    Objeto[10].GetComponent<SpriteRenderer>().color = d;
+                }
+                break;
+            case 22:
+                Oxigenio.transform.position = pontoFixoO.transform.position;
+                break;
+            case 27:
+                Oxigenio.transform.position = Objeto[13].transform.position;
+                if (Oxigenio.transform.position == Objeto[13].transform.position)
+                {
+                    Color e = green;
+                    Objeto[13].GetComponent<SpriteRenderer>().color = e;
+                }
+                break;
+            case 28:
+                Oxigenio.transform.position = pontoFixoO.transform.position;
+                break;
+            case 33:
+                Oxigenio.transform.position = Objeto[16].transform.position;
+                if (Oxigenio.transform.position == Objeto[16].transform.position)
+                {
+                    Color f = green;
+                    Objeto[16].GetComponent<SpriteRenderer>().color = f;
+                }
+                break;
+            case 34:
+                Oxigenio.transform.position = pontoFixoO.transform.position;
+                break;
+        }
+    }
+    #endregion
+    //----------------------------------------------------------------------------------------------------------------------------------------2-3-6-9-12-15
+    #region C
+    public void BotãoC()
+    {
+        switch (vl)
+        {
+            case 5:
+                Carbono.transform.position = Objeto[2].transform.position;
+                if (Carbono.transform.position == Objeto[2].transform.position)
+                {
+                    Color a = green;
+                    Objeto[2].GetComponent<SpriteRenderer>().color = a;
+                }
+                break;
+            case 6:
+                Carbono.transform.position = pontoFixoC.transform.position;
+                break;
+            case 7:
+                Carbono.transform.position = Objeto[3].transform.position;
+                if (Carbono.transform.position == Objeto[3].transform.position)
+                {
+                    Color b = green;
+                    Objeto[3].GetComponent<SpriteRenderer>().color = b;
+                }
+                break;
+            case 8:
+                Carbono.transform.position = pontoFixoC.transform.position;
+                break;
+            case 13:
+                Carbono.transform.position = Objeto[6].transform.position;
+                if (Carbono.transform.position == Objeto[6].transform.position)
+                {
+                    Color c = green;
+                    Objeto[6].GetComponent<SpriteRenderer>().color = c;
+                }
+                break;
+            case 14:
+                Carbono.transform.position = pontoFixoC.transform.position;
+                break;
+            case 19:
+                Carbono.transform.position = Objeto[9].transform.position;
+                if (Carbono.transform.position == Objeto[9].transform.position)
+                {
+                    Color d = green;
+                    Objeto[9].GetComponent<SpriteRenderer>().color = d;
+                }
+                break;
+            case 20:
+                Carbono.transform.position = pontoFixoC.transform.position;
+                break;
+            case 25:
+                Carbono.transform.position = Objeto[12].transform.position;
+                if (Carbono.transform.position == Objeto[12].transform.position)
+                {
+                    Color e = green;
+                    Objeto[12].GetComponent<SpriteRenderer>().color = e;
+                }
+                break;
+            case 26:
+                Carbono.transform.position = pontoFixoC.transform.position;
+                break;
+            case 31:
+                Carbono.transform.position = Objeto[15].transform.position;
+                if (Carbono.transform.position == Objeto[15].transform.position)
+                {
+                    Color f = green;
+                    Objeto[15].GetComponent<SpriteRenderer>().color = f;
+                }
+                break;
+            case 32:
+                Carbono.transform.position = pontoFixoC.transform.position;
+                break;
+        }
+    }
+    #endregion
+}
