16d5e19c858be0bf7d1ed830a259904f9a8f3999
HellenBorba <hellenmborba@icloud.com>
2022-09-20 17:00:11 -0300

    28
    Adicionando o Photon.

---
+++ Projeto_Pi/Assets/Scripts/Multiplayer/Lobby.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+
+public class Lobby : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        PhotonNetwork.ConnectUsingSettings();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    // public override void OnConnectedToMaster()
+}
