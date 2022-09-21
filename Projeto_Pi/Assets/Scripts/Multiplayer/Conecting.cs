35178b764eed37c30ff77f25470bd1dfc32d1c0b
HellenBorba <hellenmborba@icloud.com>
2022-09-21 09:40:08 -0300

    29
    Adicionando o MultiPlayer.

--- Projeto_Pi/Assets/Scripts/Multiplayer/Conecting.cs
+++ Projeto_Pi/Assets/Scripts/Multiplayer/Conecting.cs
@@ -1,21 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using Photon.Pun;
 using Photon.Realtime;
 
-public class Lobby : MonoBehaviour
+public class Conecting : MonoBehaviourPunCallbacks
 {
     // Start is called before the first frame update
     void Start()
     {
         PhotonNetwork.ConnectUsingSettings();
     }
-
-    // Update is called once per frame
-    void Update()
+    public override void OnConnectedToMaster()
     {
-
+        PhotonNetwork.JoinLobby();
     }
-    // public override void OnConnectedToMaster()
+    public override void OnJoinedLobby()
+    {
+        SceneManager.LoadScene("Lobby");
+    }
 }
---
+++ Projeto_Pi/Assets/Scripts/Multiplayer/CreatJoin.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Photon.Pun;
+
+public class CreatJoin : MonoBehaviourPunCallbacks
+{
+    public InputField CriarInput;
+    public InputField EntrarInput;
+
+    public void CriandoSala()
+    {
+        PhotonNetwork.CreateRoom(CriarInput.text);
+    }
+    public void EntrandoSala()
+    {
+        PhotonNetwork.JoinRoom(EntrarInput.text);
+    }
+    public override void OnJoinedRoom()
+    {
+        PhotonNetwork.LoadLevel("Game");
+    }
+}
