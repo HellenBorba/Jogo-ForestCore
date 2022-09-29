b14df342dcaf3b6a72103d64a3691ab61aa53279
HellenBorba <hellenmborba@icloud.com>
2022-09-29 17:06:15 -0300

    40
    Organizando para implementar o multiPlayer....

--- Projeto_Pi/Assets/Scripts/Multiplayer/SpawnPlayer.cs
+++ Projeto_Pi/Assets/Scripts/Multiplayer/SpawnPlayer.cs
@@ -9,25 +9,20 @@ public class SpawnPlayer : MonoBehaviour
     public GameObject Spawn;
     public GameObject spawnPosition;
     public float MaxX, MaxY, MaxZ, MinX, MinY, MinZ;
-    private PhotonView pv;
-
-    private int idPLayer;
     //----------------------------------------------------------------------------------------------------------------------------------------
     private void Start()
     {
-        if (idPLayer == 0)
+        if (PhotonNetwork.IsMasterClient)
         {
+            PhotonNetwork.NickName = ("Player1");
             Vector3 r = new Vector3(Random.Range(MinX, MinY), Random.Range(MinY, MaxY), Random.Range(MinZ, MaxZ));
             PhotonNetwork.Instantiate(PlayerPrefabs1.name, r, Quaternion.identity);
-            idPLayer++;
         }
-
-        if (pv.ViewID == 1002)
+        else
         {
+            PhotonNetwork.NickName = ("Player2");
             Vector3 m = new Vector3(Random.Range(MinX, MinY), Random.Range(MinY, MaxY), Random.Range(MinZ, MaxZ));
             PhotonNetwork.Instantiate(PlayerPrefabs2.name, m, Quaternion.identity);
-            PhotonNetwork.Instantiate(PlayerPrefabs2.name, spawnPosition.transform.position, Quaternion.identity,0);
-
         }
     }
     //----------------------------------------------------------------------------------------------------------------------------------------
